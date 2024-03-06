4eef83e91af5a959ac8f0596f896d1450422cbd9
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2024-03-06 14:47:07 +1300

    Adding updated base file (Added string checker)

--- FRC_Base_v1.py
+++ FRC_Base_v1.py
@@ -18,4 +18,30 @@ def num_checker(question, flo_int):
         except ValueError:
             print(error)
 
+# Function for checking strings
+def string_checker(question, valid_list, num_letters):
+    while True:      
+        # Error message generation
+        error = f"Please choose {valid_list[0]} or {valid_list[1]}"
+
+        # Ask user for choice (and force lowercase)
+        response = input(question).lower()
+        
+        # Runs through list and if response is an item in list (or first letter the full name is returned)
+        for item in valid_list:
+            # If 'first_letter' is set to yes then check the list for first letter and full strings
+            if num_letters > 0:
+                if response == item[:num_letters] or response == item:
+                    return item
+            # If 'first_letter' is set to no then only check the list for full strings
+            else:
+                if response == item:
+                    return item
+
+        # Output error if response not in list        
+        print(error)
+
+    print("Instructions go Here")
+    print()
+
 # Main Routine goes here
